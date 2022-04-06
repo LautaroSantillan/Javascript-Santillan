8a060c41715b5e0328e214ad2ce1a7491f86a30f
lautarosantillan <santillanlautaro03@gmail.com>
2022-04-06 00:24:16 -0300

    js basico

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,43 +1,26 @@
-// Mi proyecto final quiero que sea un carrito de compras de una tienda virtual y que tenga varios productos.
-
-// Precios de los productos iniciales
-let cocaCola = 550;
-let pepsi = 500;
-let sevenUp = 450;
-
-// Variable
-let cantidad = 0;
-
-// Funciones de operaciones matemáticas
-const resultado = x => x * cantidad;
-
-// Algoritmo
-do {
-    cantidad = parseInt(prompt("¿Cuantas Coca-Cola quiere? Si no quiere este producto o quiere seguir ponga 0"))
-    if(cantidad == "0") {
-        break
+// Declaro mi lista de productos con sus caracteristicas
+class Producto {
+    constructor(nombre, precio, volumen) {
+        this.nombre = nombre.toUpperCase();
+        this.precio = parseFloat(precio);
+        this.volumen = volumen;
     }
-    let nuevoPrecioA = resultado(cocaCola)
-    alert("Tu compra de " + cantidad + " Coca-Cola tiene un valor de " + nuevoPrecioA)
-} while(true)
-alert("Sigamos la compra...")
-
-do {
-    cantidad = parseInt(prompt("¿Cuantas Pepsi quiere? Si no quiere este producto o quiere seguir la compra ponga 0"))
-    if(cantidad == "0") {
-        break
+    // Aplico un descuento de 10%
+    descuento(){
+    this.precio = this.precio * 0.90;
     }
-    let nuevoPrecioB = resultado(pepsi)
-    alert("Tu compra de " + cantidad + " Pepsi tiene un valor de " + nuevoPrecioB)
-} while(true)
-alert("Sigamos la compra...")
+}
 
-do {
-    cantidad = parseInt(prompt("¿Cuantas 7up quiere? Si no quiere este producto o quiere terminar la compra ponga 0"))
-    if(cantidad == "0") {
-        break
-    }
-    let nuevoPrecioC = resultado(sevenUp)
-    alert("Tu compra de " + cantidad + " 7up tiene un valor de " + nuevoPrecioC)
-} while(true)
-alert("FIN DE LA COMPRA :)")
+// Formo un array para almacenar mis datos / objetos
+const productos = [];
+productos.push(new Producto("cocacola","550","2.5l"));
+productos.push(new Producto("pepsi","500","2.5l"));
+productos.push(new Producto("sevenup","450","2.5l"));
+
+// Utilizo la sentencia for...of para modificar sus precios
+for(const producto of productos)
+    producto.descuento();
+
+// Prueba de algunos metodos
+console.log(productos.length);
+console.log(productos.includes("manaos"));
