c67ec99d0e32452b1fa4fee5b1d8dc730a4e2c93
lautarosantillan <santillanlautaro03@gmail.com>
2022-04-22 01:44:19 -0300

    funcionalidad a botones de orden

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -34,10 +34,12 @@ productos.push(new producto("18", "drlemon","340","1lt", "img/drlemon.jpg"));
 productos.push(new producto("19", "termidor","200","1lt", "img/termidor.jpg"));
 productos.push(new producto("20", "quilmes","170","1lt", "img/quilmes.jpg"));
 
-// Plantillas literales e InnerHTML
+// Mostrar todos los productos
 
-let htmlCatalogo = ''
-for(const producto of productos) {
+function mostrarProductos(){
+    productos.forEach(producto => {
+    let htmlCatalogo = ''
+    for(const producto of productos) {
     htmlCatalogo += `
                     <div class="producto--ind" data-aos="zoom-in">
                         <img src="${producto.img}" alt="ImagenDe${producto.nombre}">
@@ -50,18 +52,86 @@ for(const producto of productos) {
                     </div>
                     `
     document.getElementById('productos--contenedor').innerHTML = htmlCatalogo
-}
-
-// Mostrar todos los productos
+}})}
 
 document.addEventListener('DOMContentLoaded', () => {
     mostrarProductos();
 })
 
-function mostrarProductos(){
-    productos.forEach(producto => {
-        console.log(producto.id, producto.nombre, producto.volumen, producto.precio, producto.img)
+// Filtrado de orden alfabetico A-Z
+
+const btnOrderA = document.getElementsByClassName('ordenA')
+for (const btn of btnOrderA) {
+    btn.onclick = orderAtoZ
+}
+
+function orderAtoZ(e){
+    const btn = e.target
+    const producto = productos.sort((a,b) => {
+        if(a.nombre > b.nombre) {return 1}
+        if(a.nombre < b.nombre) {return -1}
+        return 0
     })
+    console.log("Se ordeno alfabetica la lista de la A a la Z:", productos)
+    mostrarProductos();
+}
+
+// Filtrado de orden alfabetico Z-A
+
+const btnOrderZ = document.getElementsByClassName('ordenZ')
+for (const btn of btnOrderZ) {
+    btn.onclick = orderZtoA
+}
+
+function orderZtoA(e){
+    const btn = e.target
+    const producto = productos.sort((a,b) => {
+        if(a.nombre < b.nombre) {return 1}
+        if(a.nombre > b.nombre) {return -1}
+        return 0
+    })
+    console.log("Se ordeno alfabetica la lista de la Z a la A:", productos)
+    mostrarProductos();
+}
+
+// Filtrado de precios de menor a mayor
+
+const btnCheap = document.getElementsByClassName('precioBarato')
+for (const btn of btnCheap) {
+    btn.onclick = precioBarato
+}
+
+function precioBarato(e){
+    const btn = e.target
+    const producto = productos.sort((p1, p2) => {
+        if(p1.precio < p2.precio) {
+            return -1;
+        } else {
+            return 0;
+        }
+    })
+    console.log("Se ordenaron los precios de menor a mayor:", productos)
+    mostrarProductos();
+}
+
+// Filtrado de precios de mayor a menor
+
+const btnExpensive = document.getElementsByClassName('precioCaro')
+for (const btn of btnExpensive) {
+    btn.onclick = precioCaro
+}
+
+function precioCaro(e){
+    const btn = e.target
+    const producto = productos.sort((p1, p2) => {
+        if(p1.precio > p2.precio) {
+            return -1;
+        } else {
+            return 0;
+        }
+    })
+    console.log("Se ordenaron los precios de mayor a menor:", productos)
+    mostrarProductos();
 }
 
 // Carrito
@@ -84,76 +154,4 @@ function addToCart(e) {
         button: "CONTINUAR LA COMPRA",
     });
     console.log("Se agrego el producto:", producto.nombre, producto.volumen, "al carrito")
-}
-
-// Filtrado de orden alfabetico A-Z
-
-const btnOrderA = document.getElementsByClassName('ordenA')
-for (const btn of btnOrderA) {
-    btn.onclick = orderAtoZ
-}
-
-function orderAtoZ(e){
-    const btn = e.target
-    const producto = productos.sort((a,b) => {
-        if(a.nombre > b.nombre) {return 1}
-        if(a.nombre < b.nombre) {return -1}
-        return 0
-    })
-    console.log("Se ordeno alfabetica la lista de la A a la Z:", productos)
-}
-
-// Filtrado de orden alfabetico Z-A
-
-const btnOrderZ = document.getElementsByClassName('ordenZ')
-for (const btn of btnOrderZ) {
-    btn.onclick = orderZtoA
-}
-
-function orderZtoA(e){
-    const btn = e.target
-    const producto = productos.sort((a,b) => {
-        if(a.nombre < b.nombre) {return 1}
-        if(a.nombre > b.nombre) {return -1}
-        return 0
-    })
-    console.log("Se ordeno alfabetica la lista de la Z a la A:", productos)
-}
-
-// Filtrado de precios de menor a mayor
-
-const btnCheap = document.getElementsByClassName('precioBarato')
-for (const btn of btnCheap) {
-    btn.onclick = precioBarato
-}
-
-function precioBarato(e){
-    const btn = e.target
-    const producto = productos.sort((p1, p2) => {
-        if(p1.precio < p2.precio) {
-            return -1;
-        } else {
-            return 0;
-        }
-    })
-    console.log("Se ordenaron los precios de menor a mayor:", productos)
-}
-
-// Filtrado de precios de mayor a menor
-
-const btnExpensive = document.getElementsByClassName('precioCaro')
-for (const btn of btnExpensive) {
-    btn.onclick = precioCaro
-}
-
-function precioCaro(e){
-    const btn = e.target
-    const producto = productos.sort((p1, p2) => {
-        if(p1.precio > p2.precio) {
-            return -1;
-        } else {
-            return 0;
-        }
-    })
-    console.log("Se ordenaron los precios de mayor a menor:", productos)
 }
