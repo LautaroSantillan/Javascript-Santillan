2b9b7eff35b50c9f97cde10e2cd6536da11e1043
lautarosantillan <santillanlautaro03@gmail.com>
2022-04-24 21:16:50 -0300

    Funcion del Carrito de compras

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,4 +1,4 @@
-// Declaro las caracteristicas que van a tener los productos
+// CARACTERISTICAS DE LOS PRODUCTOS
 
 class producto {
     constructor(id, nombre, precio, volumen, img,) {
@@ -10,7 +10,7 @@ class producto {
     }
 }
 
-// Formo un array para almacenar mis datos / objetos
+// ARRAY DE PRODUCTOS
 
 const productos = [];
 productos.push(new producto("1", "cocacola","350","2.5lt", "img/cocacola.jpg"));
@@ -34,31 +34,31 @@ productos.push(new producto("18", "drlemon","340","1lt", "img/drlemon.jpg"));
 productos.push(new producto("19", "termidor","200","1lt", "img/termidor.jpg"));
 productos.push(new producto("20", "quilmes","170","1lt", "img/quilmes.jpg"));
 
-// Mostrar todos los productos
+// MOSTRAR PRODUCTOS
 
-function mostrarProductos(){
-    productos.forEach(producto => {
-    let htmlCatalogo = ''
-    for(const producto of productos) {
-    htmlCatalogo += `
-                    <div class="producto--ind" data-aos="zoom-in">
-                        <img src="${producto.img}" alt="ImagenDe${producto.nombre}">
-                        <h3> ${producto.nombre} </h3>
-                        <h3> ${producto.volumen} </h3>
-                        <div class="producto--precios">
-                        <p> $ ${producto.precio}</p>
-                        </div>
-                        <button type="button" class="btnAddCart" id="producto-${producto.id}">Agregar al Carrito</button>
-                    </div>
-                    `
-    document.getElementById('productos--contenedor').innerHTML = htmlCatalogo
-}})}
+// function mostrarProductos(){
+//     productos.forEach(producto => {
+//     let htmlCatalogo = ''
+//     for(const producto of productos) {
+//     htmlCatalogo += `
+//                 <div class="producto--ind item" data-aos="zoom-in">
+//                     <img src="${producto.img}" alt="ImagenDe${producto.nombre}" class="itemimg">
+//                     <h3 class="itemtitle"> ${producto.nombre} </h3>
+//                     <h3 class="itemvol"> ${producto.volumen} </h3>
+//                     <div class="producto--precios">
+//                         <p class="itemprice">$${producto.precio}</p>
+//                     </div>
+//                     <button type="button" class="btnAddCart" id="producto-${producto.id}">Agregar al Carrito</button>
+//                 </div>
+//                     `
+//     document.getElementById('productos--contenedor').innerHTML = htmlCatalogo
+// }})}
 
-document.addEventListener('DOMContentLoaded', () => {
-    mostrarProductos();
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//     mostrarProductos();
+// })
 
-// Filtrado de orden alfabetico A-Z
+// ORDEN ALFABETICO A-Z
 
 const btnOrderA = document.getElementsByClassName('ordenA')
 for (const btn of btnOrderA) {
@@ -76,7 +76,7 @@ function orderAtoZ(e){
     mostrarProductos();
 }
 
-// Filtrado de orden alfabetico Z-A
+// ORDEN ALFABETICO Z-A
 
 const btnOrderZ = document.getElementsByClassName('ordenZ')
 for (const btn of btnOrderZ) {
@@ -94,7 +94,7 @@ function orderZtoA(e){
     mostrarProductos();
 }
 
-// Filtrado de precios de menor a mayor
+// ORDEN DE PRECIOS DE MENOR A MAYOR
 
 const btnCheap = document.getElementsByClassName('precioBarato')
 for (const btn of btnCheap) {
@@ -114,7 +114,7 @@ function precioBarato(e){
     mostrarProductos();
 }
 
-// Filtrado de precios de mayor a menor
+// ORDEN DE PRECIOS DE MAYOR A MENOR
 
 const btnExpensive = document.getElementsByClassName('precioCaro')
 for (const btn of btnExpensive) {
@@ -134,24 +134,160 @@ function precioCaro(e){
     mostrarProductos();
 }
 
-// Carrito
+// CARRITO
+
+let htmlCatalogo = ''
+for(const producto of productos) {
+    htmlCatalogo += `
+                    <div class="producto--ind item" data-aos="zoom-in">
+                        <img src="${producto.img}" alt="ImagenDe${producto.nombre}" class="item-img">
+                        <h3 class="item-title"> ${producto.nombre} </h3>
+                        <h3 class="item-vol"> ${producto.volumen} </h3>
+                        <div class="producto--precios">
+                            <p class="item-price">$${producto.precio}</p>
+                        </div>
+                        <button type="button" class="btnAddCart" id="producto-${producto.id}">Agregar al Carrito</button>
+                    </div>
+                    `
+    document.getElementById('productos--contenedor').innerHTML = htmlCatalogo
+}
 
 const carrito = []
 
-const btncart = document.getElementsByClassName('btnAddCart')
-for (const btn of btncart) {
-    btn.onclick = addToCart
+const btncart = document.querySelectorAll('.btnAddCart')
+btncart.forEach(addToCart => {
+    addToCart.addEventListener('click', addToCartClicked);
+});
+
+const buyButton = document.querySelector('.comprarButton');
+buyButton.addEventListener('click', comprarButtonClicked);
+
+const shoppingCartItemsContainer = document.querySelector('.shoppingCartItemsContainer');
+
+// AGREGAR ITEM AL CARRITO (BOTON)
+
+function addToCartClicked(e){
+    const btn = e.target
+    const item = btn.closest('.item');
+
+    const itemTitle = item.querySelector('.item-title').textContent;
+    const itemVol = item.querySelector('.item-vol').textContent;
+    const itemPrice = item.querySelector('.item-price').textContent;
+    const itemImg = item.querySelector('.item-img').src;
+    
+    addItemToShoppingCart(itemTitle, itemVol, itemPrice, itemImg);
 }
 
-function addToCart(e) {
-    const btn = e.target
-    const id = btn.id.split('-')[1] 
-    const producto = productos.find(p => p.id == id)
+// AGREGAR ITEM AL CARRITO
+
+function addItemToShoppingCart(itemTitle, itemVol, itemPrice, itemImg){
+    
+    const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
+        'shoppingCartItemTitle'
+    );
+    for (let i = 0; i < elementsTitle.length; i++) {
+        if (elementsTitle[i].innerText === itemTitle) {let elementQuantity = elementsTitle[
+        i].parentElement.parentElement.parentElement.querySelector('.shoppingCartItemQuantity');
+        elementQuantity.value++;
+        $('.toast').toast('show');
+        updateShoppingCartTotal();
+        return;
+        }
+    }
+
+    const shoppingCartRow = document.createElement('div');
+    const shoppingCartContent = `
+    <div class="row shoppingCartItem">
+        <div class="col-6">
+            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <img src="${itemImg}" class="shopping-cart-image">
+                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle} ${itemVol}</h6>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number" value="1">
+                <button class="btn btn-danger buttonDelete" type="button">X</button>
+            </div>
+        </div>
+    </div>
+    `;
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    shoppingCartItemsContainer.append(shoppingCartRow);
+
+    shoppingCartRow.querySelector('.buttonDelete').addEventListener('click', removeShoppingCartItem);
+
+    shoppingCartRow.querySelector('.shoppingCartItemQuantity').addEventListener('change', quantityChanged);
+
+    updateShoppingCartTotal();
+}
+
+// ACTUALIZACION DE PRECIOS
+
+function updateShoppingCartTotal(){
+    let total = 0;
+    const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+    
+    shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+
+    shoppingCartItems.forEach(shoppingCartItem => {
+        const shoppingCartItemPriceElement = shoppingCartItem.querySelector('.shoppingCartItemPrice');
+        const shoppingCartItemPrice = Number(shoppingCartItemPriceElement.textContent.replace('$',''));
+        const shoppingCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity'); 
+        const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
+        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+        
+        shoppingCartTotal.innerHTML = `$${total}`
+        addLocalStorage();
+    })
+}
+
+// ELIMINAR ITEMS
+
+function removeShoppingCartItem(e){
+    const buttonClicked = e.target;
+    buttonClicked.closest('.shoppingCartItem').remove();
+    updateShoppingCartTotal();
+} 
+
+// CANTIDAD DE SELECCION DE PRODUCTOS
+
+function quantityChanged(e){
+    const input = e.target;
+    if(input.value<=0){
+        input.value=1;
+    }
+    updateShoppingCartTotal();
+}
+
+// BOTON COMPRAR
+
+function comprarButtonClicked(){
     swal({
-        title: "Producto Agregado!",
-        text: "¡Bien Hecho! Usted acaba de añadir un producto al carrito.",
+        title: "Compra realizada!",
+        text: "¡Bien Hecho! Usted acaba de efectuar su pedido.",
         icon: "success",
         button: "CONTINUAR LA COMPRA",
     });
-    console.log("Se agrego el producto:", producto.nombre, producto.volumen, "al carrito")
+    shoppingCartItemsContainer.innerHTML = ``;
+    updateShoppingCartTotal();
+}
+
+// LOCALSTORAGE
+
+function addLocalStorage(){
+    localStorage.setItem('.shoppingCartTotal', JSON.stringify(carrito));
+}
+
+window.onload = function(){
+    const localstorage = JSON.parse(localStorage.getItem('.shoppingCartTotal'))
+    if(localstorage){
+        carrito = localstorage;
+        renderCarrito();
+    }
 }
